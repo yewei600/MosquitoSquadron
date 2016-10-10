2ee528e844f3ba9ba9a8fd53907b66aee7e0e0b5
yewei600 <ericwei94@gmail.com>
2016-10-10 11:01:21 -0400

    before implementing REST stuff

--- app/src/main/java/com/ericwei/mosquitosquadron/MainActivity.java
+++ app/src/main/java/com/ericwei/mosquitosquadron/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ericwei.mosquitosquadron;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,10 +82,13 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_about) {
-            // Handle the camera action
+            Intent i = new Intent(MainActivity.this, AboutSquadron.class);
+            startActivity(i);
         } else if (id == R.id.nav_calendar) {
-
+            Intent i = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(i);
         } else if (id == R.id.nav_map) {
+
 
         } else if (id == R.id.nav_contact) {
 
