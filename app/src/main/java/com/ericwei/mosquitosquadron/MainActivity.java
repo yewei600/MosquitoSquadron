ab5cb72fe65c4bf7640292e80756f5ddd6f411f4
yewei600 <ericwei94@gmail.com>
2016-10-10 13:19:09 -0400

    can now get raw data from REST api

--- app/src/main/java/com/ericwei/mosquitosquadron/MainActivity.java
+++ app/src/main/java/com/ericwei/mosquitosquadron/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ericwei.mosquitosquadron;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,9 +14,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView tvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,17 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        tvData = (TextView) findViewById(R.id.tv1);
+
+        Button getData = (Button) findViewById(R.id.getData);
+        getData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String restURL = "http://frozen-savannah-70920.herokuapp.com/contacts";
+                new RestOperation().execute("http://frozen-savannah-70920.herokuapp.com/contacts");
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +66,113 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+    public class RestOperation extends AsyncTask<String, String, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+//
+//            String finalJson = buffer.toString();
+//
+//            JSONObject parentObject = new JSONObject(finalJson);
+//            JSONArray parentArray = parentObject.getJSONArray("movies");
+//
+//            List<BannerModel> BannerModelList = new ArrayList<>();
+//
+//            Gson gson = new Gson();
+//            for(int i=0; i<parentArray.length(); i++) {
+//                JSONObject finalObject = parentArray.getJSONObject(i);
+//                /**
+//                 * below single line of code from Gson saves you from writing the json parsing yourself which is commented below
+//                 */
+//                BannerModel bannerModel = gson.fromJson(finalObject.toString(), BannerModel.class);
+////                    movieModel.setMovie(finalObject.getString("movie"));
+////                    movieModel.setYear(finalObject.getInt("year"));
+////                    movieModel.setRating((float) finalObject.getDouble("rating"));
+////                    movieModel.setDirector(finalObject.getString("director"));
+////
+////                    movieModel.setDuration(finalObject.getString("duration"));
+////                    movieModel.setTagline(finalObject.getString("tagline"));
+////                    movieModel.setImage(finalObject.getString("image"));
+////                    movieModel.setStory(finalObject.getString("story"));
+////
+////                    List<MovieModel.Cast> castList = new ArrayList<>();
+////                    for(int j=0; j<finalObject.getJSONArray("cast").length(); j++){
+////                        MovieModel.Cast cast = new MovieModel.Cast();
+////                        cast.setName(finalObject.getJSONArray("cast").getJSONObject(j).getString("name"));
+////                        castList.add(cast);
+////                    }
+////                    movieModel.setCastList(castList);
+//                // adding the final object in the list
+//                movieModelList.add(movieModel);
+//            }
+//            return movieModelList;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+//        dialog.dismiss();
+//        if (result != null) {
+//            MovieAdapter adapter = new MovieAdapter(getApplicationContext(), R.layout.row, result);
+//            lvMovies.setAdapter(adapter);
+//            lvMovies.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                    MovieModel movieModel = result.get(position);
+//                    Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+//                    intent.putExtra("movieModel", new Gson().toJson(movieModel));
+//                    startActivity(intent);
+//                }
+//            });
+//        } else {
+//            Toast.makeText(getApplicationContext(), "Not able to fetch data from server, please check url.", Toast.LENGTH_SHORT).show();
+//        }
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
---
+++ app/src/main/java/com/ericwei/mosquitosquadron/RestOperation.java
@@ -0,0 +1,24 @@
+package com.ericwei.mosquitosquadron;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import com.ericwei.mosquitosquadron.models.BannerModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by eric on 10/10/16.
+ */
